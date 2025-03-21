714eb1a63a0678acdc99aee8f4d5b890c495a4b8
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-03-21 03:59:59 +0300

    extending to the top of the screen and hiding icons on the top

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <StatusBar
+        hidden={true}
+        translucent={true}
+        backgroundColor="transparent"
+      />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
